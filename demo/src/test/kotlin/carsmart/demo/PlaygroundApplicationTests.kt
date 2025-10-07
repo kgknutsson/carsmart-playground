835f49488e62315e32ed2ec76104400a25dd9f4b
Fredrik Lohmann <fredrik.lohmann@live.se>
2025-10-07 10:51:22 +0200

    feat: Demo application for playground

---
+++ demo/src/test/kotlin/carsmart/demo/PlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package carsmart.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
